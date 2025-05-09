e20c8c151158fe2be8f6c3fdcddc9ea85eeb445b
Sina Haraeeni <s.haraeeni@gmail.com>
2025-05-09 22:48:32 +0330

    Update: move file

--- docker-compose/zabbix/db_dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# RUN BEFORE UPGRADING POSTGRES IMAGE.
-docker compose down
- 
-docker compose up -d db
-docker compose exec postgres pg_dumpall -U zabbixadmin > pgdump.sql
-docker compose down
--- docker-compose/zabbix/db_restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# RUN AFTER UPGRADING POSTGRES IMAGE.
-
-docker compose up -d db
-cat pgdump.sql | docker compos postgres zabbix_db_1 psql -U zabbixadmin -d zabbix
-docker compose down
